cad15d1ef1d22fa62b28dc7ade74c07b9f6c0ae5
Ilya <logvinovIl2000@gmail.com>
2021-07-18 10:42:58 +0300

    heroku db

--- db.js
+++ db.js
@@ -10,4 +10,7 @@ module.exports = new Pool({
   password: 'bb860b34ad1b14b3c40c002fb18ec5cdc82f63b903327657bdb7a475b2ae39d9',
   port: 5432,
   idleTimeoutMillis:0,
+  ssl: {
+    rejectUnauthorized: false
+  },
 })
